8caf5e64b0ef4ab583361823aa1cafdd2a6cc620
ryofutebol <ryo.futebol@gmail.com>
2022-10-22 16:45:23 +0900

    go websocket

--- cmd/go.mod
+++ cmd/go.mod
@@ -2,8 +2,4 @@ module app
 
 go 1.18
 
-require (
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	golang.org/x/tools v0.1.12 // indirect
-)
+require golang.org/x/net v0.0.0-20220722155237-a158d28d115b
--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,61 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+
+	"golang.org/x/net/websocket"
 )
 
-func hd(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World Go")
-}
+//go:embed templates/index.html
+var indexTmpl embed.FS
 
 func main() {
-	http.HandleFunc("/", hd)
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", index)
+	http.Handle("/ws", websocket.Handler(msgHandler))
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFS(indexTmpl, "templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tmpl.Execute(w, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func msgHandler(ws *websocket.Conn) {
+	defer ws.Close()
+
+	// 初回のメッセージを送信
+	err := websocket.Message.Send(ws, "こんにちは！ :)")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		// メッセージを受信する
+		msg := ""
+		err = websocket.Message.Receive(ws, &msg)
+		log.Println(msg)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// メッセージを返信する
+		err := websocket.Message.Send(ws, fmt.Sprintf(`%q というメッセージを受け取りました。`, msg))
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
